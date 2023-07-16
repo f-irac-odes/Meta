8e97fb4f91e131ec7b5b13531f32cff7f8faa1e2
chronitch <chronitch@outlook.com>
2023-07-16 17:46:27 +0200

    continuation

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -10,23 +10,17 @@
 	import Grass from './Grass.svelte';
 	import Crate from './Crate.svelte';
 	import Fence from './Fence.svelte';
+	import Spikes from './Spikes.svelte';
 
-  let position = [0, 3, 0];
+  let x = writable 
+  let y, 
+  let z;
+  let position = ;
 
   let plane;
   let playerMesh;
 
   let testMode = false;
-
-  let texture = new THREE.TextureLoader().load('./src/lib/components/texture.png')
-  texture.wrapS = THREE.RepeatWrapping ; 
-  texture.wrapT = THREE.RepeatWrapping;
-  texture.repeat.set( 16, 16 ); 
-
-  let texture1 = new THREE.TextureLoader().load('./Meta/src/lib/components/texture.png')
-  texture1.wrapS = THREE.RepeatWrapping ; 
-  texture1.wrapT = THREE.RepeatWrapping;
-  texture1.repeat.set( 3, 3 )
 
   let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
    camera.lookAt(new THREE.Vector3(0, 0, 0));
@@ -70,7 +64,7 @@
     <AutoColliders shape="cuboid" >
     <T.Mesh  position={[0-x, 0, z]}>
       <T.BoxGeometry args={[1, 3, 1]} />
-      <T.MeshStandardMaterial color="cyan" roughness={0} map={texture1}/>
+      <T.MeshStandardMaterial color="cyan" roughness={0} />
     </T.Mesh>
     </AutoColliders>
     {:else if block === 2}
@@ -84,6 +78,10 @@
       <T.BoxGeometry args={[1, 0.2, 1]} />
       <T.MeshStandardMaterial color="cyan" transparent opacity={0.5}/>
     </T.Mesh>
+    {:else if block === 6}
+      <Fence position={[0-x, -0.5, z]} rotated />
+    {:else if block === 7}
+      <Spikes position={[0-x, -0.5, z]}/>   
     {:else if  block === 1}
    <Player position={position} {playerMesh}/>
     {/if}
@@ -95,7 +93,7 @@
 <AutoColliders shape={'cuboid'} >
 <T.Mesh position={[0, -1, 0]} bind:ref={plane} receiveShadow>
   <T.BoxGeometry args={[98,1, 98]} />
-  <T.MeshStandardMaterial color="cyan" roughness={-1} map={texture}/>
+  <T.MeshStandardMaterial color="cyan" roughness={-1}/>
 </T.Mesh>
 </AutoColliders>
 
